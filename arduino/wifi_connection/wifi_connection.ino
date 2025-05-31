f26169dbf1fa24f3ebf9eb2243ea26bbde4cd6ac
tatiana.dalbusco <tatiana.dalbusco@student.supsi.ch>
2025-05-31 13:53:45 +0200

    Final Arduino Code

--- arduino/wifi_connection/wifi_connection.ino
+++ arduino/wifi_connection/wifi_connection.ino
@@ -24,11 +24,13 @@ int fsrPinA5Value = 0;
 // Chair identifier
 const String IDchair = "CHAR01"; 
 
-String date_time = "";
+// It is calculated on the server
 String posture = "unknown";
 
+String date_time = "";
 unsigned long lastRead = 0;
 const unsigned long interval = 1000;
+
 
 /*  - Initializes serial, Wi-Fi connection and time sync
     - Check if there is WiFi connection
@@ -60,30 +62,39 @@ void setup() {
 
 
 /*  Checks if the ESP32 is currently connected to a Wi-Fi network
-    - If not connected, it attempt to reconnect to the specified hotspot using SSID and password
+    - If not connected, it attempt to connect to the specified hotspot using SSID and password
     - Tries up to 20 times with 500ms delay between each attempt
     - Prints the connection result and ESP32's assigned IP address
     parameters: none
     return: void
     */
 void checkWiFiConnection() {
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("WiFi disconnected. Attempting to reconnect..."); // Debug
 
+  // Check if the ESP32 is not connected to Wi-Fi
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi disconnected. Attempting to connect..."); // Debug
+
+    // Force a disconnection to reset the Wi-Fi state
     WiFi.disconnect(true);
     delay(1000);
+
+    // Attempt to connect to the hotspot using predefined SSID and password
     WiFi.begin(ssidHotspot, passwordHotspot);
 
     int retry = 0;
+
+    // Try to connect for up to 20 attempts (approx. 10 seconds)
     while (WiFi.status() != WL_CONNECTED && retry < 20) {
       delay(500);
       Serial.print(".");
       retry++;
     }
 
+    // Check if connection was successful
     if (WiFi.status() == WL_CONNECTED) {
       Serial.println("\nConnected to WiFi. IP ESP32: " + WiFi.localIP().toString()); // Debug
-    } else {
+    } 
+    else {
       Serial.println("\nFailed to connect to HOTSPOT after 20 attempts!."); // Debug
     }
   }
@@ -126,7 +137,6 @@ String handleJSON() {
 }
 
 
-
 /*  Sends JSON data to a server via HTTP POST
       - Tries to connect to the IP server (local)
       - Prints server response or error
@@ -139,11 +149,18 @@ void sendDataToServer(String jsonData) {
   if (WiFi.status() == WL_CONNECTED) {
 
     HTTPClient http;
-    String serverURL = "http://" + serverIP + ":3000/chair";
-    Serial.println("Using saved server IP: " + serverURL);  // Debug
 
+    // Construct the server URL using the saved IP address
+    String serverURL = "http://" + serverIP + ":3000/chair";
+
+    // Start the HTTP connection with the server using the specified URL
     http.begin(serverURL);
+
+    // Set a timeout of 5 seconds for the server to respond
+    // If the server doesn't reply within this time, the request will fail
     http.setTimeout(5000);
+
+    // Add a header to tell the server that we are sending JSON data
     http.addHeader("Content-Type", "application/json");
 
     // Send JSON data via POST and store the HTTP response code
@@ -151,19 +168,25 @@ void sendDataToServer(String jsonData) {
     Serial.print("HTTP Response Code: "); // Debug
     Serial.println(httpResponseCode);     // Debug
     
-    // httpResponseCode == 0 → server not reachable (wrong IP, server down, or network issue)
+    // If the response code is positive, server responded successfully
     if (httpResponseCode > 0) {
+
+      // Get and print the response from the server
       String response = http.getString();
       Serial.println("Server response: " + response); // Debug
-    } else {
+    } 
+    else {
       Serial.printf("Error sending to %s. Error: %s\n",
                     serverIP.c_str(),
                     http.errorToString(httpResponseCode).c_str());
+
+      // Close the connection to free resources
       http.end();
       delay(100);
     }
     
-  } else {
+  } 
+  else {
     Serial.println("WiFi not connected!"); // Debug
   }
 }
@@ -178,9 +201,10 @@ void sendDataToServer(String jsonData) {
     */
 void loop() {
   
+  // Make sure the ESP32 is connected to Wi-Fi
   checkWiFiConnection();
 
-  // Read sensors at fixed time intervals
+  // Check if the defined time interval has passed before reading sensors again
   unsigned long currentMillis = millis();
   if (currentMillis - lastRead >= interval) {
     lastRead = currentMillis;
